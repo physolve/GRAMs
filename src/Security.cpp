b067a2b1b077f17b6cceef886e92050868b84e98
physolve <mityahaleev555@gmail.com>
2024-03-05 19:04:16 +0700

    Add security class for valve and pressure mask

--- src/Security.cpp
+++ src/Security.cpp
@@ -1,19 +1,92 @@
 #include "Security.h"
 
+ValveGraph::ValveGraph(const QString &selfName): m_selfName(selfName) {}
+void ValveGraph::addEachInList(const QStringList &nodeValveList){
+    checkEachInList = true;
+    nodeValves = nodeValveList;
+}
+void ValveGraph::addRuleOfThree(const QString &threeNodeOne,const QString &threeNodeTwo){
+    checkRuleOfThree = true;
+    m_threeNodeOne = threeNodeOne;
+    m_threeNodeTwo = threeNodeTwo;
+}
+bool ValveGraph::applyGraphMask(const QMap<QString, bool> &valveMap){
+    if(!valveMap[m_selfName]) 
+        return;
+    bool resultEach = true;
+    if(checkEachInList)
+        resultEach = maskEachInList(valveMap);
+    bool resultRuleOfThree = true;
+    if(checkRuleOfThree)
+        resultRuleOfThree = maskRuleOfThree(valveMap);
+    bool result = resultEach*resultRuleOfThree;
+    return result;
+}
+
+ValveToRangePressure::ValveToRangePressure(const QString &selfName, double pressureOpen, double pressureClose)
+: m_selfName(selfName), m_pressureOpen(pressureOpen), m_pressureClose(pressureClose) {}
+bool ValveToRangePressure::applyPressureMask(bool state, double currentPressure, double incomingPressure){
+    bool m_state = state;
+    if(currentPressure>=m_pressureClose&&incomingPressure>=m_pressureClose){
+        m_state = false;
+    }
+    else if(currentPressure<=m_pressureOpen&&incomingPressure<=m_pressureOpen)
+        m_state = true;
+    return state;
+}
+
+ValveToSafePressure::ValveToSafePressure(const QString &selfName, double pressureOpen)
+: m_selfName(selfName), m_pressureOpen(pressureOpen) {}
+bool ValveToSafePressure::applyPressureMask(bool state, double currentPressure){
+    bool m_state = state;
+    if(currentPressure>=m_pressureOpen)
+        m_state = true;
+    return m_state;
+}
+
 Security::Security(QObject *parent) :
     QObject(parent)
 {
 
 }
 
-void Security::applyProfile(){
-    
-}
-
-void Security::setContradictionValves(const QVariantMap &contradictionValves){
+void Security::setContradictionValves(const QVariantMap &contradictionValves, const QVariantList &ruleOfThreeList){
     // valve graph security option from QStringlist values of contradictionValves
+    QStringList valves = contradictionValves.keys();
+    for(auto valve : valves){
+        m_contradictionValves.insert(valve, ValveGraph(valve));
+        m_contradictionValves[valve].addEachInList(contradictionValves[valve].toStringList());
+    }
+    for(auto ruleOfThreeVariant : ruleOfThreeList){
+        auto allThree = ruleOfThreeVariant.toStringList();
+        for(auto oneOfThree: allThree){
+            if(!m_contradictionValves.contains(oneOfThree))
+                m_contradictionValves.insert(oneOfThree, ValveGraph(oneOfThree));
+            auto twoOfThree = ruleOfThreeVariant.toStringList();
+            twoOfThree.removeOne(oneOfThree);
+            m_contradictionValves[oneOfThree].addRuleOfThree(twoOfThree.at(0),twoOfThree.at(1));
+        }
+    }
 }
 
-void Security::checkValveAction(){
+void Security::setRangePressureValves(const QVariantMap &rangePressureValves){
+    QStringList valves = rangePressureValves.keys();
+    for(auto valve : valves){
+        auto pressureRange = rangePressureValves[valve].toMap();
+        double pressureOpen = pressureRange["pressureOpen"].toDouble();
+        double pressureClose = pressureRange["pressureClose"].toDouble();
+        m_rangePressureValves.insert(valve, ValveToRangePressure(valve, pressureOpen, pressureClose));
+    }
+}
+void Security::setSafePressureValves(const QVariantMap &safePressureValves){
+    QStringList valves = safePressureValves.keys();
+    for(auto valve : valves){
+        auto pressureRange = safePressureValves[valve].toMap();
+        double pressureOpen = pressureRange["pressureOpen"].toDouble();
+        m_safePressureValves.insert(valve, ValveToSafePressure(valve, pressureOpen));
+    }
+}
 
+void Security::checkValveAction(const QMap<QString, bool> &valveMap){
+    
 }
--- src/Security.h
+++ src/Security.h
@@ -2,19 +2,67 @@
 
 #include <QVariant>
 
-struct ValveGraph{
-    //security options and twoOfThreeRule
+class ValveGraph{
+public:
+    ValveGraph(const QString & selfName);
+    void addEachInList(const QStringList &nodeValveList);
+    void addRuleOfThree(const QString &threeNodeOne,const QString &threeNodeTwo);
+    
+    bool applyGraphMask(const QMap<QString, bool> &valveMap);
+    
+private:
+    const QString m_selfName;
+    bool checkEachInList = false;
+    QStringList nodeValves;
+    bool maskEachInList(const QMap<QString, bool> &valveMap){
+        for(auto nodeValve : nodeValves){
+            if(valveMap[nodeValve]){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkRuleOfThree = false;
+    QString m_threeNodeOne;
+    QString m_threeNodeTwo;
+    bool maskRuleOfThree(const QMap<QString, bool> &valveMap){
+        if(valveMap[m_threeNodeOne]&&valveMap[m_threeNodeTwo])
+            return false;
+        return true;
+    }
+};
+
+class ValveToRangePressure{
+public:
+    ValveToRangePressure(const QString &selfName, double pressureOpen, double pressureClose);
+    bool applyPressureMask(bool state, double currentPressure, double incomingPressure);
+private:
+    const QString m_selfName;
+    //const QString m_quartile;
+    const double m_pressureOpen;
+    const double m_pressureClose;
+};
+
+class ValveToSafePressure{
+public:
+    ValveToSafePressure(const QString &selfName, double pressureOpen);
+    bool applyPressureMask(bool state, double currentPressure);
+private:
+    const QString m_selfName;
+    //const QString m_quartile;
+    const double m_pressureOpen;
 };
 
 class Security : public QObject
 {
     Q_OBJECT
-    Q_INVOKABLE void setContradictionValves(const QVariantMap &contradictionValves);
+    Q_INVOKABLE void setContradictionValves(const QVariantMap &contradictionValves, const QVariantList &ruleOfThreeList);
+    Q_INVOKABLE void setRangePressureValves(const QVariantMap &rangePressureValves);
+    Q_INVOKABLE void setSafePressureValves(const QVariantMap &safePressureValves);
 public:
     Security(QObject *parent = 0);
-
-    void applyProfile();
-    void checkValveAction();
+    void checkValveAction(const QMap<QString, bool> &valveMap);
+    void checkValvePressure(const QMap<QString, bool> &valveMap, const QMap<QString, double> &pressureMap);
 
 private:
     // current states (valves)
@@ -24,4 +72,6 @@ private:
     // filter incoming states to current states
     // filter incoming states to current/incoming pressure
     QMap<QString, ValveGraph> m_contradictionValves;
+    QMap<QString, ValveToRangePressure> m_rangePressureValves;
+    QMap<QString, ValveToSafePressure> m_safePressureValves;
 };
